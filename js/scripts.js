b03dcd2a81eee63a048b5858d773e3dde23f655b
Kevin Williams <kevin@k9w.org>
2022-01-22 22:05:59 -0800

    Begin refactoring HTML and JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,28 @@
+// Business Logic
+
+
+
+// User Interface Logic
+
 $(document).ready(function () {
-  $("form.where-will-the-code-run_form").submit(function (event) {
+  $("form#whereWillTheCodeRun").submit(function (event) {
     event.preventDefault();
-    const where-will-the-code-run = $("input.where-will-the-code-run_answer").val();
-
-      if (where-will-the-code-run === "mobile-friendly-website") {
-        $('.mobile-friendly-website').show();
-        $('.mobile-app').hide();
-        $('.both-website-and-mobile-app').hide();
-        $('.neither-website-nor-mobile-app').hide();
-        $('.where-will-the-code-run_no-answer').hide();
-
-      }
-
-      if (where-will-the-code-run === "mobile-app") {
-        $('.mobile-friendly-website').hide();
-        $('.mobile-app').show();
-        $('.both-website-and-mobile-app').hide();
-        $('.neither-website-nor-mobile-app').hide();
-        $('.where-will-the-code-run_no-answer').hide();
-
-      }
-
-      if (where-will-the-code-run === "both-website-and-mobile-app") {
-        $('.mobile-friendly-website').hide();
-        $('.mobile-app').hide();
-        $('.both-website-and-mobile-app').show();
-        $('.neither-website-nor-mobile-app').hide();
-        $('.where-will-the-code-run_no-answer').hide();
-
-      }
-
-      elseif (where-will-the-code-run === "neither-website-nor-mobile-app") {
-        $('.mobile-friendly-website').hide();
-        $('.mobile-app').hide();
-        $('.both-website-and-mobile-app').hide();
-        $('.neither-website-nor-mobile-app').show();
-        $('.where-will-the-code-run_no-answer').hide();
-
-      }
-
-      else {
-        $('.mobile-friendly-website').hide();
-        $('.mobile-app').hide();
-        $('.both-website-and-mobile-app').hide();
-        $('.neither-website-nor-mobile-app').hide();
-        $('.where-will-the-code-run_no-answer').show();
+    const answerWhereCodeRuns = $("input:radio[name=choiceWhereCodeRuns]:checked").val();
+    let result1;
+    if (answerWhereCodeRuns === "mobileFriendlyWebsite") {
+        $("#whereWillTheCodeRun").hide();
+        $("#mobileFriendlyWebsite").show();
+    } else if (answerWhereCodeRuns === "mobile-app") {
+        $("#whereWillTheCodeRun").hide();
+        $('#mobileApp').show();
+    } else if (answerWhereCodeRuns === "bothWebsiteAndMobileApp") {
+        $("#whereWillTheCodeRun").hide();
+        $("#bothWebsiteAndMobileApp").show();
+    } else if (answerWhereCodeRuns === "neitherWebsiteNorMobileApp") {
+        $("#whereWillTheCodeRun").hide();
+        $("#neitherWebsiteNorMobileApp").show();
       }
     
   });
 });
 
-$(document).ready(function() {
-  $("form#quiz").submit(function(event) {
-    event.preventDefault();
-    const age = parseInt($("input#age").val());
-    const hand = $("select#hand").val();
-
-    let match = (100 - age) * 3;
-    if (hand === "right" && age < 26) {
-      match += 50;
-    }
-    
-    if (favoriteFood === "pizza" ) {
-      match += 15;
-    }
-  
-    if (favoriteColor === "turquoise" ) {
-      match += 15;
-    }
-  
-  });
-});
